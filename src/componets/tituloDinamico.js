da86c613d7e697e87cb9e6f416064fa76b0a7f3c
nowadraco <Nowadraco@gmail.com>
2023-02-07 22:42:15 +0000

    finalizado titulo dinâmico

--- src/componets/tituloDinamico.js
+++ src/componets/tituloDinamico.js
@@ -5,7 +5,7 @@ class tituloDinamico extends HTMLElement {
     const shadow = this.attachShadow({ mode: "open"});
     
     const componetRoot = document.createElement("h1");
-    componetRoot.textContent = this.getAttribute("titulo")
+    componetRoot.textContent = this.getAttribute("titulo");
     
     const style = document.createElement("style");
     style.textContent = `
@@ -14,8 +14,8 @@ class tituloDinamico extends HTMLElement {
     }
     `
     
-    shadow.appendChild(componetRoot)
-    shadow.appendChild(style)
+    shadow.appendChild(componetRoot);
+    shadow.appendChild(style);
     
   }
 }
