df3907256518f4070668861d8c8c4328f616de0f
nowadraco <Nowadraco@gmail.com>
2023-02-08 00:23:18 +0000

    configurando atributos para cardnews

--- src/componets/cardnews.js
+++ src/componets/cardnews.js
@@ -1,9 +1,46 @@
 class cardNews extends HTMLElement {
   constructor(){
     super();
+
     const shadow = this.attachShadow({ mode: "open"});
-    shadow.innerHTML = "<h1>hello word<h1/>"
+    shadow.appendChild(this.build());
+    shadow.appendChild(this.styles());
   }
+
+  build() {
+    const componetRoot = document.createElement("div");
+    componetRoot.setAttribute("class", "card");
+
+    const cardLeft = document.createElement("div");
+    cardLeft.setAttribute("class", "card__Left");
+
+    const autor = document.createElement("span");
+    const linktitle = document.createElement("a");
+    const newscontent = document.createElement("p");
+
+    cardLeft.appendChild(autor);
+    autor.textContent = "by " + (this.getAttribute("autor") || "anonymous");
+
+    cardLeft.appendChild(linktitle);
+    linktitle.textContent = this.getAttribute("title")
+
+    cardLeft.appendChild(newscontent);
+    newscontent.textContent = this.getAttribute("contet")
+
+
+    const cardRight = document.createElement("div");
+    cardRight.setAttribute("class", "card__Right");
+    const newsimage = document.createElement("img")
+    cardRight.appendChild(newsimage);
+
+
+    componetRoot.appendChild(cardLeft);
+    componetRoot.appendChild(cardRight);
+
+    return componetRoot
+  }
+
+  styles() {}
 }
 
 customElements.define('card-news',cardNews);
